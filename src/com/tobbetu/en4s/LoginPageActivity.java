300439acb5a02c0c2540d75cf2875fa8ea851ab5
Mustafa Simav <ms@msimav.net>
2013-06-07 18:10:48 +0300

    login implemented, BasicHTTPClient works with cookies

--- src/com/tobbetu/en4s/LoginPageActivity.java
+++ src/com/tobbetu/en4s/LoginPageActivity.java
@@ -1,12 +1,19 @@
 package com.tobbetu.en4s;
 
+import java.io.IOException;
+
+import com.tobbetu.en4s.backend.Login;
+import com.tobbetu.en4s.backend.Login.LoginFailedException;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -16,115 +23,158 @@ import android.widget.Toast;
 
 public class LoginPageActivity extends Activity {
 
-	private Button bLogin = null;
-	private EditText etUsername, etPassword;
-	
-	private String sharedFileName = "loginInfo";
-	protected static SharedPreferences loginPreferences;
-	
-	private LocationManager lManager = null;
-	private double latitude = 0;
-	private double longitude = 0;
-	
-	private boolean flag = false;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    private Button bLogin = null;
+    private EditText etUsername, etPassword;
 
-//		startService(new Intent(this, EN4SService.class));
-		
-		lManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-		LocationListener mlocListener = new LoginPageLocationListener();
-		lManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0,
-				mlocListener);
-		
-		
-		//Burada sessionID ile HTTP POST yapilacak, olumlu donerse giris olacak
-		//olumsuz donerse kullanici adi ve sifre ile yeni bir baglanti gerceklestirilecek ve 
-		//yeni sessionID guncellenecek.
-			
-		setContentView(R.layout.activity_login_page);
-		getActionBar().hide();
-		etUsername = (EditText) findViewById(R.id.etUsername);
-		etPassword = (EditText) findViewById(R.id.etPassword);
-		
-		bLogin = (Button) findViewById(R.id.bLogin);
-		bLogin.setOnClickListener(loginButtonListener);
-		
-	}
-	
-	OnClickListener loginButtonListener = new OnClickListener() {
-		public void onClick(View v) {
+    private String sharedFileName = "loginInfo";
+    protected static SharedPreferences loginPreferences;
 
-			if(etUsername.getText().toString().equals("") || etPassword.getText().toString().equals("")) {
-				Toast.makeText(getApplicationContext(), "Missing content, please fill username field !", Toast.LENGTH_SHORT).show();
-			} else {
-				
-				/* Share Share Share Preferences */
-				SharedPreferences.Editor preferencesEditor = loginPreferences.edit();
-				preferencesEditor.putString("username", etUsername.getText().toString());
-				preferencesEditor.putString("password", etPassword.getText().toString());
-				//=======Session ID ==================================================================
-				preferencesEditor.putString("sessionID", "sessionID");
-				//=======Session ID ==================================================================				
-				preferencesEditor.apply();
-				/* Share Share Share Preferences */
-							
-				//unique bir kullanici mi ?
-				Intent i = new Intent(LoginPageActivity.this, MainActivity.class);
-				i.putExtra("latitude", latitude);
-				i.putExtra("longitude", longitude);
-				startActivity(i);
-			}
-		}
-	};
+    private LocationManager lManager = null;
+    private double latitude = 0;
+    private double longitude = 0;
 
-	protected void onPause() {
-		super.onPause();
-		finish();
-	};
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.login_page, menu);
-		return true;
-	}
-	
-	class LoginPageLocationListener implements LocationListener {
+    private boolean flag = false;
 
-		@Override
-		public void onLocationChanged(Location arg0) {
-			
-			latitude = arg0.getLatitude();
-			longitude = arg0.getLongitude();		
-			
-			loginPreferences = getSharedPreferences(sharedFileName, MODE_PRIVATE);
-			if((loginPreferences.getAll().size() != 0) && flag == false){
-				
-				flag = true;
-				
-				Intent i = new Intent(LoginPageActivity.this, MainActivity.class);
-				i.putExtra("latitude", latitude);
-				i.putExtra("longitude", longitude);
-				startActivity(i);
-			}
-			
-		}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		@Override
-		public void onProviderDisabled(String arg0) {
-		}
+        // startService(new Intent(this, EN4SService.class));
 
-		@Override
-		public void onProviderEnabled(String arg0) {
-		}
+        lManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        LocationListener mlocListener = new LoginPageLocationListener();
+        lManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0,
+                mlocListener);
 
-		@Override
-		public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
-		}
-		
-	}
+        // Burada sessionID ile HTTP POST yapilacak, olumlu donerse giris olacak
+        // olumsuz donerse kullanici adi ve sifre ile yeni bir baglanti
+        // gerceklestirilecek ve
+        // yeni sessionID guncellenecek.
+
+        setContentView(R.layout.activity_login_page);
+        getActionBar().hide();
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+
+        bLogin = (Button) findViewById(R.id.bLogin);
+        bLogin.setOnClickListener(loginButtonListener);
+
+    }
+
+    OnClickListener loginButtonListener = new OnClickListener() {
+        public void onClick(View v) {
+
+            if (etUsername.getText().toString().equals("")
+                    || etPassword.getText().toString().equals("")) {
+                Toast.makeText(getApplicationContext(),
+                        "Missing content, please fill username field !",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+
+                /* Share Share Share Preferences */
+                SharedPreferences.Editor preferencesEditor = loginPreferences
+                        .edit();
+                preferencesEditor.putString("username", etUsername.getText()
+                        .toString());
+                preferencesEditor.putString("password", etPassword.getText()
+                        .toString());
+                // =======Session ID
+                // ==================================================================
+                preferencesEditor.putString("sessionID", "sessionID");
+                // =======Session ID
+                // ==================================================================
+                preferencesEditor.apply();
+                /* Share Share Share Preferences */
+
+                // unique bir kullanici mi ?
+                Intent i = new Intent(LoginPageActivity.this,
+                        MainActivity.class);
+                i.putExtra("latitude", latitude);
+                i.putExtra("longitude", longitude);
+                startActivity(i);
+            }
+        }
+    };
+
+    protected void onPause() {
+        super.onPause();
+        finish();
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.login_page, menu);
+        return true;
+    }
+
+    class LoginPageLocationListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location arg0) {
+
+            latitude = arg0.getLatitude();
+            longitude = arg0.getLongitude();
+
+            loginPreferences = getSharedPreferences(sharedFileName,
+                    MODE_PRIVATE);
+            if ((loginPreferences.getAll().size() != 0) && flag == false) {
+
+                flag = true;
+
+                Intent i = new Intent(LoginPageActivity.this,
+                        MainActivity.class);
+                i.putExtra("latitude", latitude);
+                i.putExtra("longitude", longitude);
+                new LoginTask().execute(
+                        loginPreferences.getString("username", ""),
+                        loginPreferences.getString("password", ""));
+                startActivity(i);
+            }
+
+        }
+
+        @Override
+        public void onProviderDisabled(String arg0) {
+        }
+
+        @Override
+        public void onProviderEnabled(String arg0) {
+        }
+
+        @Override
+        public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+        }
+
+    }
+
+    class LoginTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... arg0) {
+            String username = arg0[0];
+            String passwd = arg0[1];
+            Login newLogin = new Login(username, passwd);
+            try {
+                newLogin.makeRequest();
+            } catch (IOException e) {
+                // TODO user have no internet connection
+                Log.e(getClass().getName(), "IOException", e);
+
+            } catch (LoginFailedException e) {
+                // TODO inform user
+                Log.e(getClass().getName(), String.format(
+                        "[Login Failed] username: %s, passwd: %s", username,
+                        passwd), e);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            // TODO kanil will implement here
+        }
+
+    }
 
 }
---
+++ src/com/tobbetu/en4s/backend/Login.java
@@ -0,0 +1,38 @@
+package com.tobbetu.en4s.backend;
+
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+public class Login {
+
+    JSONObject loginInfo;
+
+    public Login(String username, String passwd) {
+        this.loginInfo = new JSONObject();
+        try {
+            this.loginInfo.put("username", username);
+            this.loginInfo.put("password", passwd);
+        } catch (JSONException e) {
+            Log.e(getClass().getName(), "JSONException", e);
+        }
+    }
+
+    public void makeRequest() throws IOException, LoginFailedException {
+        HttpResponse loginResponse = Requests.post(
+                "http://en4s.msimav.net/login", loginInfo.toString());
+        if (loginResponse.getStatusLine().getStatusCode() == HttpStatus.SC_NOT_FOUND)
+            throw new LoginFailedException();
+    }
+
+    public class LoginFailedException extends Exception {
+
+        private static final long serialVersionUID = 2996222505192491564L;
+
+    }
+}
