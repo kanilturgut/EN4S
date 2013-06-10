654c597b99d4157c15ab59365f8382757dc13164
Mustafa Simav <ms@msimav.net>
2013-06-10 22:44:12 +0300

    image upload implemented at backend

--- src/com/tobbetu/en4s/backend/Image.java
+++ src/com/tobbetu/en4s/backend/Image.java
@@ -1,11 +1,19 @@
 package com.tobbetu.en4s.backend;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.R.string;
 import android.graphics.Bitmap;
+import android.util.Base64;
 import android.util.Log;
 
 public class Image {
@@ -42,6 +50,32 @@ public class Image {
             Log.e(getClass().getName(), "NoSuchAlgorithmException throwed", e);
         }
         return hash;
+    }
+
+    private String base64image() {
+        return Base64.encodeToString(bitmapToByteArray(this.bmp),
+                Base64.DEFAULT);
+    }
+
+    private String toJSON() {
+        JSONObject image = new JSONObject();
+        try {
+            image.put("hash", sha256hash());
+            image.put("pic", base64image());
+        } catch (JSONException e) {
+            Log.e("Complaint.toJSON", "Unexpected JSONException", e);
+        }
+        return image.toString();
+    }
+
+    public void upload(String oid) throws IOException {
+        Log.d("[JSON]", this.toJSON());
+        HttpResponse post = Requests.post("http://en4s.msimav.net/upload/" + oid,
+                this.toJSON());
+        if (!Requests.checkStatusCode(post, HttpStatus.SC_CREATED)) {
+            // TODO throw exception
+            Log.d(getClass().getName(), "Status Code in not 201");
+        }
     }
 
 }
