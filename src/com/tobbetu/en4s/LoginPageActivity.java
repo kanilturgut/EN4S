7d5bfd9bc10834d21926b8f4d3cb3d97979e394d
Kadir Anil Turgut <kanilturgut@gmail.com>
2013-08-02 16:24:17 +0300

    Login with facebook and login with username oldu gibi, sirada feed etme islemleri var

--- src/com/tobbetu/en4s/LoginPageActivity.java
+++ src/com/tobbetu/en4s/LoginPageActivity.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.util.Arrays;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.location.Location;
@@ -21,6 +23,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.FacebookException;
@@ -39,11 +42,11 @@ import com.tobbetu.en4s.backend.Login.LoginFailedException;
 public class LoginPageActivity extends Activity {
 
 	private String TAG = "LoginPageActivity";
-	
+
 	private Button bLogin = null;
 	private EditText etUsername, etPassword;
 	private ProgressBar pbLogin = null;
-	
+
 	private LoginButton faceButton = null;
 	private String faceAccessToken = null;
 
@@ -58,6 +61,11 @@ public class LoginPageActivity extends Activity {
 	private boolean flag = false;
 	private boolean loginFlag = false;
 	private boolean locationFlag = false;
+	
+	private boolean intentCreated = false;
+	
+    private AlertDialog alertDialog = null;
+    private boolean closeFlag = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -83,58 +91,74 @@ public class LoginPageActivity extends Activity {
 
 			bLogin = (Button) findViewById(R.id.bLogin);
 			bLogin.setOnClickListener(loginButtonListener);
-			
+
 			pbLogin = (ProgressBar) findViewById(R.id.pbLogin);
+			faceButton = (LoginButton) findViewById(R.id.faceButton);
 
 			loginPreferences = getSharedPreferences(sharedFileName,
 					MODE_PRIVATE);
 			if ((loginPreferences.getAll().size() != 0) && flag == false) {
 				flag = true;
-				
-				new LoginTask().execute(
-						loginPreferences.getString("username", ""),
-						loginPreferences.getString("password", ""));
+//				if(!loginPreferences.getString("facebook_accessToken", "null").equals("null")) {
+//					//facebook login
+//					Log.d(TAG, "trying login with facebook");
+//				}
+//				else { //normal login
+					Log.d(TAG, "trying login with username");
+					Log.d(TAG, loginPreferences.getString("username", ""));
+					Log.d(TAG, loginPreferences.getString("password", ""));
+					new LoginTask().execute(
+							loginPreferences.getString("username", ""),
+							loginPreferences.getString("password", ""));
+//				}
 			}
-			
-			faceButton = (LoginButton) findViewById(R.id.faceButton);
+
 			faceButton.setOnErrorListener(new OnErrorListener() {
-				
+
 				@Override
 				public void onError(FacebookException error) {
 					Log.i(TAG, "Error " + error.getMessage());					
 				}
 			});
-			
+
 			//facebook izinlerini set ediyoruz.
 			faceButton.setReadPermissions(Arrays.asList("basic_info", "email"));
-			
+
 			faceButton.setSessionStatusCallback(new StatusCallback() {
-				
+
 				@Override
 				public void call(Session session, SessionState state, Exception exception) {
-					
+
 					if (session.isOpened()) {
 						faceAccessToken = session.getAccessToken();
 						Log.i(TAG, "Access Token" + session.getAccessToken());
-						
+
 						Request.executeMeRequestAsync(session, new GraphUserCallback() {
-							
+
 							@Override
 							public void onCompleted(GraphUser user, Response response) {
 								if (user != null) {
+
+									SharedPreferences.Editor spEditor = loginPreferences.edit();
+									spEditor.putString("facebook_name", user.asMap().get("name").toString());
+									spEditor.putString("facebook_username", user.asMap().get("username").toString());
+									spEditor.putString("facebook_email", user.asMap().get("email").toString());
+									spEditor.putString("facebook_accessToken", faceAccessToken);
+									spEditor.apply();
+
 									String userID = user.getId();
 									String name = user.asMap().get("name").toString();
 									String username = user.asMap().get("username").toString();
 									String email = user.asMap().get("email").toString();
-									
+
 									Log.i(TAG, userID + "," + name + "," + username + "," + email);
-									
+
 								}
 							}
 						});
-						
+
 					}
-					
+
 				}
 			});
 		}
@@ -161,17 +185,31 @@ public class LoginPageActivity extends Activity {
 				/* Share Share Share Preferences */
 
 				// unique bir kullanici mi ?
-								
+
 				new LoginTask().execute(etUsername.getText().toString(),
 						etPassword.getText().toString());
 			}
 		}
 	};
 
-	protected void onPause() {
-		super.onPause();
-//		finish();
-	};
+	@Override
+	protected void onStop() {
+		super.onStop();
+		
+		if (intentCreated)
+			finish();
+		
+		Log.d(TAG, closeFlag + "");
+		
+		if (alertDialog != null)
+			alertDialog.dismiss();
+	}
+	
+	@Override
+	public void onBackPressed() {
+		Log.e(TAG, "in onBackPressed");
+		createAlert();		
+	}
 
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -179,7 +217,7 @@ public class LoginPageActivity extends Activity {
 		Session.getActiveSession().onActivityResult(this, requestCode,
 				resultCode, data);
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -219,14 +257,14 @@ public class LoginPageActivity extends Activity {
 		protected void onPreExecute() {
 			lockToComponents();
 		}
-		
+
 		@Override
 		protected String doInBackground(String... arg0) {
 			String username = arg0[0];
 			String passwd = arg0[1];
 
-            Log.d(getClass().getName(), "username: " + username);
-            Log.d(getClass().getName(), "passwd: " + passwd);
+			Log.d(getClass().getName(), "username: " + username);
+			Log.d(getClass().getName(), "passwd: " + passwd);
 
 			Login newLogin = new Login(username, passwd);
 			try {
@@ -267,13 +305,15 @@ public class LoginPageActivity extends Activity {
 	}
 
 	private void startIntent() {
-	    Log.d(getClass().getName(), String.format("Location: %s, Login: %s",
-	            locationFlag, loginFlag));
+		Log.d(getClass().getName(), String.format("Location: %s, Login: %s",
+				locationFlag, loginFlag));
 		if (locationFlag == true && loginFlag == true) {
 
+			intentCreated = true;
+			
 			//after login, we need to stop location listener
 			lManager.removeUpdates(mlocListener);
-			
+
 			Intent i = new Intent(LoginPageActivity.this, MainActivity.class);
 			i.putExtra("latitude", latitude);
 			i.putExtra("longitude", longitude);
@@ -284,12 +324,50 @@ public class LoginPageActivity extends Activity {
 
 		} 
 	}
-	
+
 	private void lockToComponents() {
-		etUsername.setVisibility(View.INVISIBLE);
-		etPassword.setVisibility(View.INVISIBLE);
-		bLogin.setVisibility(View.INVISIBLE);
-		pbLogin.setVisibility(View.VISIBLE);
+		etUsername.setVisibility(EditText.INVISIBLE);
+		etPassword.setVisibility(EditText.INVISIBLE);
+		bLogin.setVisibility(Button.INVISIBLE);
+		pbLogin.setVisibility(ProgressBar.VISIBLE);
+		faceButton.setVisibility(Button.INVISIBLE);
 
 	}	
+	
+	private void createAlert() {
+		
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Exit ?");
+		builder.setMessage("Do you really want to quit ?");
+		builder.setCancelable(true);
+		builder.setPositiveButton("OK", 
+				new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+                try {
+                	closeFlag = true;
+					System.exit(0);
+				} catch (Throwable e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            }
+        });
+		builder.setNegativeButton("Cancel", 
+				new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+                try {
+                	closeFlag = true;
+					dialog.dismiss();
+				} catch (Throwable e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            }
+        });
+		
+		alertDialog = builder.create();
+		alertDialog.show();
+	}
 }
