dee05c8eee7fa889d2e20eef10bfecddd630dee6
Mustafa Simav <ms@msimav.net>
2013-08-17 23:31:29 +0300

    comment gorme eklendi, duzeltilmeye ihtiyaci var

--- src/com/tobbetu/en4s/MoreCommentsActivity.java
+++ src/com/tobbetu/en4s/MoreCommentsActivity.java
@@ -1,55 +1,89 @@
 package com.tobbetu.en4s;
 
+import java.io.IOException;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.tobbetu.en4s.backend.Comment;
 import com.tobbetu.en4s.backend.Complaint;
 
 public class MoreCommentsActivity extends Activity {
 
-	Complaint complaint = null;
-	LatLng userPosition = null;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_more_comments);
-		
-		getActionBar().hide();
-		
-		/*Detay sayfasina geri dondugumuz zaman bu bilgileri inten icine yerlestirmeliyiz.*/
-		complaint = (Complaint) getIntent().getSerializableExtra("class");
-		userPosition = new LatLng(getIntent().getDoubleExtra("latitude", 0), getIntent().getDoubleExtra("longitude", 0));
-		
-		//Geri tusuna basinca gerceklesecek islemler
-		findViewById(R.id.bBackToDetail).setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				
-				Intent i = new Intent(MoreCommentsActivity.this, DetailsActivity.class);
-				i.putExtra("class", complaint);
-                i.putExtra("latitude", userPosition.latitude);
-                i.putExtra("longitude", userPosition.longitude);
-                startActivity(i);				
-				
-			}
-		});
-		
-		/*******************************************************************/
-		String sporDallari[] = {"Basketbol", "Futbol", "Tenis", "Voleybol",
-	            "Hentbol", "Yüzme", "Golf"};
-		ListView lvMoreComments = (ListView) findViewById(R.id.lvCommentOnMoreComment);
-		lvMoreComments.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,sporDallari));
-		
-		/*Su anda commentler serverdan alinamadigi icin bu sekilde sacma yorumlar getiriyorum. Fakat hem comment adapter
-		 * hem comment layout hem de comment sinifi yazildi. Server dan comment alinabildigi zaman burasi update edilecek*/
-	}
+    Complaint complaint = null;
+    LatLng userPosition = null;
+    private ListView lvMoreComments;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more_comments);
+
+        getActionBar().hide();
+
+        /*
+         * Detay sayfasina geri dondugumuz zaman bu bilgileri inten icine
+         * yerlestirmeliyiz.
+         */
+        complaint = (Complaint) getIntent().getSerializableExtra("class");
+        userPosition = new LatLng(getIntent().getDoubleExtra("latitude", 0),
+                getIntent().getDoubleExtra("longitude", 0));
+
+        // Geri tusuna basinca gerceklesecek islemler
+        findViewById(R.id.bBackToDetail).setOnClickListener(
+                new OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+
+                        Intent i = new Intent(MoreCommentsActivity.this,
+                                DetailsActivity.class);
+                        i.putExtra("class", complaint);
+                        i.putExtra("latitude", userPosition.latitude);
+                        i.putExtra("longitude", userPosition.longitude);
+                        startActivity(i);
+
+                    }
+                });
+
+        lvMoreComments = (ListView) findViewById(R.id.lvCommentOnMoreComment);
+        new CommentAsyncTask().execute();
+
+        /*
+         * Su anda commentler serverdan alinamadigi icin bu sekilde sacma
+         * yorumlar getiriyorum. Fakat hem comment adapter hem comment layout
+         * hem de comment sinifi yazildi. Server dan comment alinabildigi zaman
+         * burasi update edilecek
+         */
+    }
+
+    private class CommentAsyncTask extends
+            AsyncTask<String, String, List<Comment>> {
+
+        @Override
+        protected List<Comment> doInBackground(String... arg0) {
+            try {
+                return Comment.getComments(complaint);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(List<Comment> result) {
+            // FIXMEE: I have no idea which context to use, so selected randomly
+            lvMoreComments.setAdapter(new CommentListAdapter(getBaseContext(),
+                    result));
+        }
+    }
 
 }
--- src/com/tobbetu/en4s/backend/Comment.java
+++ src/com/tobbetu/en4s/backend/Comment.java
@@ -7,56 +7,125 @@
  */
 package com.tobbetu.en4s.backend;
 
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
 public class Comment {
 
-	private String id;
-	private String author;
-	private String text;
+    private String id;
+    private String author;
+    private String text;
 
-	private int upVote;
-	private int downVote;
+    private Date date;
 
-	// for future
-	// private Image userAvatar;
+    private int like;
+    private int dislike;
 
-	public String getId() {
-		return id;
-	}
+    // for future
+    // private Image userAvatar;
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public String getAuthor() {
-		return author;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public void setAuthor(String author) {
-		this.author = author;
-	}
+    public String getAuthor() {
+        return author;
+    }
 
-	public String getText() {
-		return text;
-	}
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 
-	public void setText(String text) {
-		this.text = text;
-	}
+    public String getText() {
+        return text;
+    }
 
-	public int getUpVote() {
-		return upVote;
-	}
+    public void setText(String text) {
+        this.text = text;
+    }
 
-	public void setUpVote(int upVote) {
-		this.upVote = upVote;
-	}
+    public int getLike() {
+        return like;
+    }
 
-	public int getDownVote() {
-		return downVote;
-	}
+    public void setLike(int like) {
+        this.like = like;
+    }
 
-	public void setDownVote(int downVote) {
-		this.downVote = downVote;
-	}
+    public int getDislike() {
+        return dislike;
+    }
+
+    public void setDislike(int dislike) {
+        this.dislike = dislike;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    private static Comment fromJSON(JSONObject elem) {
+        Comment obj = new Comment();
+
+        obj.setId(elem.optString("_id"));
+        obj.setAuthor(elem.optString("author"));
+        obj.setText(elem.optString("author"));
+        obj.setLike(elem.optInt("like"));
+        obj.setDislike(elem.optInt("dislike"));
+
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS");
+        try {
+            obj.setDate(df.parse(elem.optString("date")));
+        } catch (ParseException e) {
+            Log.e("Comment.fromJSON",
+                    "Date Parse Error: " + elem.optString("date"), e);
+        }
+
+        return obj;
+    }
+
+    public static List<Comment> getComments(Complaint c) throws IOException {
+        List<Comment> list = new LinkedList<Comment>();
+
+        HttpResponse get = Requests.get("http://en4s.msimav.net/comments/"
+                + c.getId());
+
+        if (!Requests.checkStatusCode(get, HttpStatus.SC_OK))
+            Log.e("Complaint.getHotList", "[ERROR] Status Code: "
+                    + get.getStatusLine().getStatusCode());
+        String response = Requests.readResponse(get);
+
+        try {
+            JSONArray results = new JSONArray(response);
+            for (int i = 0; i < results.length(); i++) {
+                JSONObject item = results.getJSONObject(i);
+                list.add(Comment.fromJSON(item));
+            }
+        } catch (JSONException e) {
+            Log.e("Complaint.parseList", "Unexpected JSON Error", e);
+        }
+
+        return list;
+    }
 
 }
