c2e748d1aaf03f4b89e091e0cdd71e2f40a38759
Mustafa Simav <ms@msimav.net>
2013-10-08 20:58:40 +0300

    Notification will be shown when sending complaint
    - twitter style notification implemented
    - notification action should be implemented
    - better enforce icon needed
    - error case must be considered

--- src/com/tobbetu/en4s/tasks/SaveComplaintTask.java
+++ src/com/tobbetu/en4s/tasks/SaveComplaintTask.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 
 import org.json.JSONException;
 
+import android.app.Activity;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -20,11 +23,37 @@ public class SaveComplaintTask extends BetterAsyncTask<Void, Complaint> {
     private Context context = null;
     private Complaint complaint = null;
     private Image image = null;
+    private NotificationManager mNotifyMgr;
+    private int notificationID;
 
     public SaveComplaintTask(Context c, Complaint cmp, Image img) {
         this.context = c;
         this.complaint = cmp;
         this.image = img;
+        this.notificationID = cmp.getTitle().hashCode();
+        this.mNotifyMgr = (NotificationManager) context
+                .getSystemService(Activity.NOTIFICATION_SERVICE);
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
+                context)
+                .setSmallIcon(R.drawable.logo)
+                .setLargeIcon(image.getBmp())
+                // TODO duzgun enforce iconu
+                .setContentTitle(complaint.getTitle())
+                .setTicker(
+                        context.getResources().getString(
+                                R.string.nc_cat_sending))
+                .setAutoCancel(false)
+                .setContentText(
+                        context.getResources().getString(
+                                R.string.nc_cat_sending));
+
+        mNotifyMgr.notify(notificationID, mBuilder.build());
     }
 
     @Override
@@ -36,10 +65,19 @@ public class SaveComplaintTask extends BetterAsyncTask<Void, Complaint> {
     @Override
     protected void onSuccess(Complaint result) {
 
-        Toast.makeText(context, R.string.nc_accepted, Toast.LENGTH_SHORT)
-                .show();
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
+                context)
+                .setSmallIcon(R.drawable.logo)
+                .setContentTitle(result.getTitle())
+                .setContentText(
+                        context.getResources().getString(R.string.nc_accepted))
+                .setAutoCancel(true).setLargeIcon(image.getBmp())
+                // TODO duzgun enforce iconu
+                .setStyle(
+                        new NotificationCompat.BigPictureStyle()
+                                .bigPicture(image.getBmp()));
 
-        // TODO implement notification (mustafa)
+        mNotifyMgr.notify(notificationID, mBuilder.build());
     }
 
     @Override
